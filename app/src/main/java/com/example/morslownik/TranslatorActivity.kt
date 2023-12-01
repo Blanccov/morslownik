3c0a4a5eeae5678a9e524deaa2bbf03b0dd898f9
Blanccov <d.madej572@gmail.com>
2023-12-01 10:53:46 +0100

    first commit

---
+++ app/src/main/java/com/example/morslownik/TranslatorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.morslownik
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class TranslatorActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.translatorlayout)
+    }
+}
