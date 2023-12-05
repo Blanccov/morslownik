553efb0977ace60bdbb87faf0cc6757f16ddbd84
Blanccov <d.madej572@gmail.com>
2023-12-05 22:27:58 +0100

    photo scanner -added

--- app/src/main/java/com/example/morslownik/CaptureActivity.kt
+++ app/src/main/java/com/example/morslownik/CaptureActivity.kt
@@ -1,46 +1,78 @@
 package com.example.morslownik
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.morslownik.ui.theme.MorslownikTheme
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+import java.io.IOException
 
 class CaptureActivity : ComponentActivity() {
+    private var imageUri: Uri? = null
+    private lateinit var textRecognizer: TextRecognizer
+    private lateinit var clear: ImageButton
+    private lateinit var getImage: ImageButton
+    private lateinit var next: ImageButton
+    private lateinit var recgText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MorslownikTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.capturelayout)
+
+        clear = findViewById(R.id.clearButton)
+        getImage = findViewById(R.id.getImageButton)
+        next = findViewById(R.id.nextButton)
+        recgText = findViewById(R.id.recgText)
+
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        getImage.setOnClickListener{
+            ImagePicker.with(this)
+                .crop()	    			//Crop image(Optional), Check Customization for more option
+                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MorslownikTheme {
-        Greeting("Android")
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                imageUri = data.data // Przypisanie wartości imageUri
+                Toast.makeText(this, "image selected", Toast.LENGTH_SHORT).show()
+                recognizeText()
+            }
+        } else {
+            Toast.makeText(this, "image not selected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun recognizeText() {
+        if (imageUri != null) {
+            try {
+                val inputImage = InputImage.fromFilePath(this, imageUri!!)
+
+                textRecognizer.process(inputImage)
+                    .addOnSuccessListener { visionText ->
+                        val resultText = visionText.text
+                        recgText.text = resultText
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                    }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/morslownik/MainActivity.kt
+++ app/src/main/java/com/example/morslownik/MainActivity.kt
@@ -2,26 +2,22 @@ package com.example.morslownik
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.morslownik.ui.theme.MorslownikTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menulayout)
 
         findViewById<ImageButton>(R.id.translator_btn).setOnClickListener {
             val intentT = Intent(applicationContext, TranslatorActivity::class.java)
+            startActivity(intentT)
+        }
+
+        findViewById<ImageButton>(R.id.cameraButton).setOnClickListener {
+            val intentT = Intent(applicationContext, CaptureActivity::class.java)
             startActivity(intentT)
         }
     }
